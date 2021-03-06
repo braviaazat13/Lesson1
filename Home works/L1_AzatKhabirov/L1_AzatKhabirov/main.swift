2bf18910bb85d80240e22cb8551033d60b5e1663
Azat Khabirov <279733@gmail.com>
2021-03-06 22:37:56 +0300

    First task

--- Home works/L1_AzatKhabirov/L1_AzatKhabirov/main.swift
+++ Home works/L1_AzatKhabirov/L1_AzatKhabirov/main.swift
@@ -7,5 +7,49 @@
 
 import Foundation
 
-print("Hello, World!")
+
+//TACK 1
+
+let numberA: Double = 2
+let nubmerB: Double = 5
+let numberC: Double = -7
+
+
+//Discriminant search
+
+let discriminant: Double
+var firstStep: Double = pow(nubmerB, 2)
+var secondStep: Double = 4 * numberA * numberC
+discriminant = firstStep - secondStep
+
+
+// Solution
+
+var answer: Double
+let firstValue: Double
+let secondValue: Double
+
+
+if discriminant < 0 {
+    print("Корней нет")
+}
+
+else if discriminant == 0 {
+    answer = (-(nubmerB)) / 2 * numberA
+    print("Корень уравнения равен: " + String(answer))
+}
+
+else {
+    firstStep = (-(nubmerB) - (sqrt(discriminant)))
+    secondStep = 2 * numberA
+    answer = firstStep / secondStep
+    print("Корень X1: " + String(answer))
+    
+    firstStep = (-(nubmerB) + (sqrt(discriminant)))
+    secondStep = 2 * numberA
+    answer = firstStep / secondStep
+    print("Корень X2: " + String(answer))}
+
+
+
 
